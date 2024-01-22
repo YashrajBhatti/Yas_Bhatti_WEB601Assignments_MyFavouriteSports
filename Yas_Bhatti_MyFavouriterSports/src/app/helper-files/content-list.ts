582467daa0bb4b051845f8b24fc8e2e47f292247
YashrajBhatti <w0832320@myscc.ca>
2024-01-21 19:02:51 -0500

    Error solved

--- Yas_Bhatti_MyFavouriterSports/src/app/helper-files/content-list.ts
+++ Yas_Bhatti_MyFavouriterSports/src/app/helper-files/content-list.ts
@@ -1,35 +1,44 @@
-import { Content } from './content-interface';
+import {Content} from './content-interface';
 
 export class ContentList {
-  private contents: Content[];
+    private _items: Content[] = [];
 
-  constructor() {
-    this.contents = [];
-  }
+    constructor() {
+        this._items = [];
+    }
 
-  get contentList(): Content[] {
-    return this.contents;
-  }
+    get content(): Content[] {
+        return this._items;
+    }
 
-  addContent(content: Content): void {
-    this.contents.push(content);
-  }
+    public addContent(Content: Content): void {
+        this._items.push(Content);
+    }
 
-  get contentCount(): number {
-    return this.contents.length;
-  }
+    public getNumberOfItem(): number {
+        return this._items.length;
+    }
 
-  getContentHtml(index: number): string {
-    const content = this.contents[index];
-    const imgHtml = content.imgURL ? `<img src="${content.imgURL}" alt="${content.title}" />` : '';
-    return `
-      <div>
-        <h2>${content.title}</h2>
-        <p>${content.description}</p>
-        <p>Creator: ${content.creator}</p>
-       <img> ${imgHtml}  </img>
-        <p>Type: ${content.type || '-'}</p>
-      </div>
-    `;
-  }
+    public getContent(index: number): string {
+        const contentItem = this._items[index];
+        var numOfItems = this.getNumberOfItem();
+        if(index < 0 || index > numOfItems) {
+            const errMsg = '<p style="color: red">Invalid Index</p>';
+
+            return `<div> ${errMsg} </div>`;
+        }
+        else {
+            
+            var title = contentItem.title;
+            var description = contentItem.description;
+            var imgURL = contentItem.imgURL;
+            var creator = contentItem.creator;
+            var type = contentItem.type;
+            var tags = contentItem.tags;
+            var imgTag = imgURL ? `<img src="${imgURL}" alt="${title}">` : '';
+        
+            return `<div> <h3>${title}</h3> <p>${description}</p> <p>Creator: ${creator}</p> ${imgTag} <p>Type: ${type}</p> <p> Tags: ${tags} </p> </div><hr/>`;
+
+        }
+    }
 }
